cdb5ce385b3befb54e43d50af802782470fa3371
coyote000451 <38084973+coyote000451@users.noreply.github.com>
2018-04-10 08:26:12 -0600

    Add files via upload

---
+++ Collections/Collections/Collections.cpp
@@ -0,0 +1,81 @@
+// Collections.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm> //for sort and count
+
+using namespace std;
+
+
+int main()
+{
+    
+	vector<int> vi;
+	for (int i = 0; i < 10; i++)
+	{
+		vi.push_back(i);
+	}
+
+	for (auto item : vi)
+	{
+		cout << item << " ";
+	}
+	cout << endl;
+
+	vector<string> vs;
+
+	cout << "enter three words ";
+	for (int i = 0; i < 3; i++)
+	{
+		string s;
+		cin >> s;
+		vs.push_back(s);
+	}
+
+	for (auto item : vs)
+	{
+		cout << item << " ";
+	}
+	cout << endl;
+
+	cout << "int vector vi has " << vi.size() << " elements" << endl;
+
+	vi[5] = 3;
+	vi[6] = -1;
+	vi[1] = 99;
+
+	for (auto item : vi)
+	{
+		cout << item << " ";
+	}
+
+	for (unsigned int i = 0; i < vi.size(); i++)
+	{
+		cout << vi[i] << " ";
+	}
+	cout << endl;
+
+	for (auto i = begin(vi); i != end(vi); i++)
+	{
+		cout << *i << " ";
+	}
+	cout << endl;
+
+	sort(begin(vs), end(vs));
+	for (auto item : vs)
+	{
+		cout << item << " ";
+	}
+	cout << endl;
+
+	int threes = count(begin(vi), end(vi), 3); //count how "3"
+	cout << "vector of ints has " << threes << " element";
+
+	int tees = count(begin(vs[0]), end(vs[0]), 't');
+	cout << "first word has " << tees << " letter t's";
+	return 0;
+}
+
